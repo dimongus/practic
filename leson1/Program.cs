dbbd241b68c00b93cff58e2e83a27f5d3b34c359
dmitrii <dima1994.dl@gmail.com>
2022-12-01 21:11:51 +0900

    first program

--- leson1/Program.cs
+++ leson1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, чтобы она выдавала его квадрат(число умноженное само на себяя)
+//например 4 - 16
+
+
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(" Вы ввели число -    ");
+Console.WriteLine(number);
+
+int result = number * number;
+
+Console.WriteLine(result);
