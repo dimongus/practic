9d19f05f058d7fd6957308c39a3aa689709ae87a
dmitrii <dima1994.dl@gmail.com>
2022-12-04 18:31:16 +0900

    add practic5

--- leson1/pract5/Program.cs
+++ leson1/pract5/Program.cs
@@ -1,3 +1,26 @@
-﻿// Выдает случайное чилсо из отрезка 10,99 и показывает наибольшшее числр
+﻿// Выдает случайное чилсо из отрезка 10,99 и показывает наибольшшее цифру числа
 //78-8
 
+Random randon = new Random();
+
+int randomNamber = randon.Next(10, 100);
+
+int lastDigit = randomNamber % 10;
+
+int digit = randomNamber / 10 % 10;
+
+Console.WriteLine(randomNamber);
+Console.WriteLine(digit);
+Console.WriteLine(lastDigit);
+
+
+if (lastDigit > digit)
+{
+    
+    Console.WriteLine("vic".lastDigit);
+}
+
+else 
+{
+    Console.WriteLine(digit);
+}
