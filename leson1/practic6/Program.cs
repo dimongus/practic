76205fac145a185ff23219802592dc56fb37db0d
dmitrii <dima1994.dl@gmail.com>
2022-12-04 19:18:56 +0900

    add practic6

---
+++ leson1/practic6/Program.cs
@@ -0,0 +1,13 @@
+﻿//выводит случвйное 3х значное число и удаляет вторую цифру числа
+
+
+Random random = new Random();
+int number = random.Next(100,1000);
+
+Console.WriteLine(number);
+
+int firstDitgit = number / 100;
+int lastDitgit = number % 10;
+int newDitgit = firstDitgit * 10 + lastDitgit;
+
+Console.WriteLine(newDitgit);
