4ccd81f15336592d1aa68edba2e1c776f77ab8a3
dmitrii <dima1994.dl@gmail.com>
2022-12-04 13:07:49 +0900

    seminar2

---
+++ leson1/pract4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+//456->6 782->2 918->8
+
+Console.WriteLine("ВВедите число");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 1000 && num > 99)
+{
+    int result = num % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Нельзя такое число");
+}
